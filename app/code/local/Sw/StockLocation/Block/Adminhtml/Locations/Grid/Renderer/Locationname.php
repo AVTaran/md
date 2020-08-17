ced6fbbc6de54011b4c6a025d2d5e8bd01226980
AutoRun <mail@stepweb.ru>
2020-08-17 22:05:07 +1200

    a small adjustment in the getLocationName

--- app/code/local/Sw/StockLocation/Block/Adminhtml/Locations/Grid/Renderer/Locationname.php
+++ app/code/local/Sw/StockLocation/Block/Adminhtml/Locations/Grid/Renderer/Locationname.php
@@ -1,13 +1,10 @@
 <?php
 
-class Sw_StockLocation_Block_Adminhtml_Locations_Grid_Renderer_Locationname
-    extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
-{
-   // protected $_values;
+class Sw_StockLocation_Block_Adminhtml_Locations_Grid_Renderer_Locationname extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract {
 
-    public function render(Varien_Object $row) {
+	public function render(Varien_Object $row) {
 		$helper = Mage::helper('swstocklocation');
 		$html = $helper->getLocationName($row->getData()['id'], false);
-        return $html;
-    }
+		return $html;
+	}
 }
